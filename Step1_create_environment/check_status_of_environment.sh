f31828c6766a6f7c7ddafe173b94d4b53cbee655
Evan Woods <efwoods@g.clemson.edu>
2018-05-13 13:36:24 -0400

    step 1 complete

---
+++ Step1_create_environment/check_status_of_environment.sh
@@ -0,0 +1,5 @@
+#Check the environment status periodically until you see a status of active.
+
+#Issue a call to the GET /v1/environments/{environment_id} method to retrieve the status of your environment. Replace {username}, {password}, and {environment_id} with your information:
+
+curl -u "9a07247a-21ec-4aee-a8ed-4bce6ba9e36e":"BOBOzbwxJyCj" "https://gateway.watsonplatform.net/discovery/api/v1/environments/622177b8-ffe3-4fdd-b1ab-75d6e46a3d25?version=2017-11-07"
