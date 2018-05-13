d70062693d472d6f050cd37649d383a327448264
Evan Woods <efwoods@g.clemson.edu>
2018-05-13 15:28:19 -0400

    step5 added

---
+++ Step5_query_collection/query.sh
@@ -0,0 +1,3 @@
+#Finally, use the GET /v1/environments/{environment_id}/collections/{collection_id}/query method to search your collection of documents.
+
+#The following example returns all entities that are called IBM. Replace {username}, {password}, {environment_id} and {configuration_id} with your information:
