91d99aaf4a003ada96a40232def6b4ca8229696d
Evan Woods <efwoods@g.clemson.edu>
2018-05-13 15:26:19 -0400

    added all documents need to start step 5

--- Step4_upload_documents/add_document_to_collection.sh
+++ Step4_upload_documents/add_document_to_collection.sh
@@ -2,4 +2,4 @@
 
 #Use the POST /v1/environments/{environment_id}/collections/{collection_id}/documents method:
 
-curl -X POST -u "9a07247a-21ec-4aee-a8ed-4bce6ba9e36e":"BOBOzbwxJyCj" -F "file=@Celgene and IBM Watson Health Forge Collaboration Designed to Transform Patient Safety Monitoring.html" https://gateway.watsonplatform.net/discovery/api/v1/environments/622177b8-ffe3-4fdd-b1ab-75d6e46a3d25/collections/38938952-3018-4190-9354-6582a0fc7cfe/documents?version=2017-11-07
+curl -X POST -u "9a07247a-21ec-4aee-a8ed-4bce6ba9e36e":"BOBOzbwxJyCj" -F "file=@test-doc4.html" https://gateway.watsonplatform.net/discovery/api/v1/environments/622177b8-ffe3-4fdd-b1ab-75d6e46a3d25/collections/38938952-3018-4190-9354-6582a0fc7cfe/documents?version=2017-11-07
